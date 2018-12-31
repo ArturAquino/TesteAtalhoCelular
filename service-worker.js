398d77bc7cf04af272b7be96c3bc2e4afa9c67ae
ArturAquino <uchihaartur@yahoo.com.br>
2018-12-31 12:03:19 -0200

    Uncomment service worker function to cache informations

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
 // // use a cacheName for cache versioning
-// var cacheName = 'v1:static';
+var cacheName = 'v1:static';
 
-// // during the install phase you usually want to cache static assets
-// self.addEventListener('install', function (e) {
-//     // once the SW is installed, go ahead and fetch the resources to make this work offline
-//     e.waitUntil(
-//         caches.open(cacheName).then(function (cache) {
-//             return cache.addAll([
-//                 './',
-//                 './css/fireworks.css',
-//                 './index.html'
-//             ]).then(function () {
-//                 self.skipWaiting();
-//             });
-//         })
-//     );
-// });
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function (e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll([
+                './',
+                './css/fireworks.css',
+                './index.html'
+            ]).then(function () {
+                self.skipWaiting();
+            });
+        })
+    );
+});
 
 // // when the browser fetches a url
 // self.addEventListener('fetch', function (event) {
