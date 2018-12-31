ebc60309b7aa6803cefe83804c28ccfaa04777c1
ArturAquino <uchihaartur@yahoo.com.br>
2018-12-31 16:06:33 -0200

    Create a neu function on service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 // // use a cacheName for cache versioning
-var cacheName = 'v1:static';
+var MyCacheName = 'v1:static';
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function (e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(MyCacheName).then(function (cache) {
             return cache.addAll([
                 './',
                 './css/fireworks.css',
@@ -17,17 +17,31 @@ self.addEventListener('install', function (e) {
     );
 });
 
-// // when the browser fetches a url
-// self.addEventListener('fetch', function (event) {
-//     // either respond with the cached object or go ahead and fetch the actual url
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             if (response) {
-//                 // retrieve from cache
-//                 return response;
-//             }
-//             // fetch as normal
-//             return fetch(event.request);
-//         })
-//     );
-// });
+// Clear cache on activate
+this.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith('v1:static')))
+                    .filter(cacheName => (cacheName !== MyCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// when the browser fetches a url
+self.addEventListener('fetch', function (event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
