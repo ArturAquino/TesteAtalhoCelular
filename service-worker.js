c5d47c0028d59e30763fa237f7f859735edbec31
ArturAquino <uchihaartur@yahoo.com.br>
2019-01-02 16:00:48 -0200

    Insert function to capture user action

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function (e) {
 });
 
 // Clear cache on activate
-this.addEventListener('activate', event => {
+self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
@@ -44,4 +44,22 @@ self.addEventListener('fetch', function (event) {
             return fetch(event.request);
         })
     );
+});
+
+self.addEventListener('beforeinstallprompt', function (e) {
+    // beforeinstallprompt Event fired
+
+    // e.userChoice will return a Promise.
+    // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+    e.userChoice.then(function (choiceResult) {
+
+        console.log(choiceResult.outcome);
+
+        if (choiceResult.outcome == 'dismissed') {
+            console.log('User cancelled home screen install');
+        }
+        else {
+            console.log('User added to home screen');
+        }
+    });
 });
