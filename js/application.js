fee03d474880adbe3cff998534337a7023071ba3
ArturAquino <uchihaartur@yahoo.com.br>
2019-01-16 11:27:38 -0200

    New adjustment of application.js

--- js/application.js
+++ js/application.js
@@ -1,63 +1,56 @@
-"use strict"; //https://love2dev.com/blog/javascript-strict-mode/
+	"use strict"; //https://love2dev.com/blog/javascript-strict-mode/
 
-(function () {
+	(() => {
 
-  var deferredPrompt;
+	var deferredPrompt;
 
-  window.addEventListener('beforeinstallprompt', function (e) {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
+	window.addEventListener('beforeinstallprompt', function (e) {
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
 
-    showAddToHomeScreen();
+		showAddToHomeScreen();
 
-  });
+	});
 
-function showAddToHomeScreen() {
+	function showAddToHomeScreen() {
 
-  var a2hsBtn = document.getElementById("ad2hs-prompt");
+		var a2hsBtn = document.getElementById("ad2hs");
+		a2hsBtn.style.display = "flex";
+		a2hsBtn.addEventListener("click", addToHomeScreen);
+	}
 
-  a2hsBtn.style.display = "flex";
+	function addToHomeScreen() {
 
-  a2hsBtn.addEventListener("click", addToHomeScreen);
+		if (deferredPrompt) {
+			// Show the prompt
+			deferredPrompt.prompt();
 
-}
+			// Wait for the user to respond to the prompt
+			deferredPrompt.userChoice
+				.then(function (choiceResult) {
 
-  function addToHomeScreen() {
+				if (choiceResult.outcome === 'accepted') {
+					console.log('User accepted the A2HS prompt');
+					var a2hsBtn = document.getElementById("ad2hs");
+					// hide our user interface that shows our A2HS button
+					a2hsBtn.style.display = 'none';
+				} else {
+					console.log('User dismissed the A2HS prompt');
+				}
 
-    var a2hsBtn = document.getElementById("ad2hs-prompt");
+				deferredPrompt = null;
 
-    // hide our user interface that shows our A2HS button
-    a2hsBtn.style.display = 'none';
+			});
+		}
+	}
 
-    if (deferredPrompt) {
-      // Show the prompt
-      deferredPrompt.prompt();
+	showAddToHomeScreen();
 
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice
-        .then(function (choiceResult) {
-
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-
-          deferredPrompt = null;
-
-        });
-
-    }
-
-  }
-
-  showAddToHomeScreen();
-
-  window.addEventListener('appinstalled', function (evt) {
-    console.log('a2hs', 'installed');
-  });
+	window.addEventListener('appinstalled', function (evt) {
+		console.log('a2hs', 'installed');
+	});
 
 
-})();
+	})();
